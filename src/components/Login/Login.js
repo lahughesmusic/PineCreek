49ab82b27392b688411728954d280e5f29d4e91b
Lea Hughes <lahughesmusic@gmail.com>
2018-09-02 17:29:00 -0500

    add files

---
+++ src/components/Login/Login.js
@@ -0,0 +1,33 @@
+ 
+//  import "../../index.css";
+
+//  <h1>PINE CREEK CASE FILES</h1>
+//  <br>
+ 
+//  <div>
+//    <img src='../images/pinecreekUSE.png'>
+
+//  </div>
+
+//  <br>
+//  <br>
+//  <br>
+//  <br>
+
+//  /* <form>
+//      <div id="border">
+//          <div class="form-group" >
+//            <label for="exampleInputEmail1">User Name</label>
+//            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Officer ">
+//            <small id="emailHelp" class="form-text text-muted"></small>
+//          </div>
+//          <div class="form-group">
+//            <label for="exampleInputPassword1">Password</label>
+//            <br>
+//            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+//          </div>
+//          <br>
+//          <button id='submit'>SUBMIT</button>
+//      </div>
+//        </form> */
+//        <h1>O S V L E</h1>
