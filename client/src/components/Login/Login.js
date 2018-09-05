e1151b3d36ee28bcd5bc01f31e7a25de917a0d0e
Lea Hughes <lahughesmusic@gmail.com>
2018-09-05 15:31:33 -0500

    add files

---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,38 @@
+ import React from 'react';
+ import "../../Login.css";
+
+ const Login = props => (
+
+ <h1>PINE CREEK CASE FILES</h1>
+ <br>
+ 
+ <div>
+   <img src='../images/pinecreekUSE.png'>
+
+ </div>
+
+ <br>
+ <br>
+ <br>
+ <br>
+
+<form>
+     <div id="border">
+         <div class="form-group" >
+           <label for="exampleInputEmail1">User Name</label>
+           <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Officer ">
+           <small id="emailHelp" class="form-text text-muted"></small>
+         </div>
+         <div class="form-group">
+           <label for="exampleInputPassword1">Password</label>
+           <br>
+           <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+         </div>
+         <br>
+         <button id='submit'>SUBMIT</button>
+     </div>
+       </form>
+
+
+)
+       
