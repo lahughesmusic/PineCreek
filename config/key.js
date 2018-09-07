46e32bade3d127c200d2d4acfdd4e1c2a1758fc0
Lea Hughes <lahughesmusic@gmail.com>
2018-09-07 13:21:04 -0500

    add files

--- config/key.js
+++ config/key.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://lahughesmusic:lahughesmusic87@ds147942.mlab.com:47942/userandpassword',
+    mongoURI: 'mongodb://mowgli:mowgli87@ds147942.mlab.com:47942/userandpassword',
     secretOrKey: 'mowgli'
 }
