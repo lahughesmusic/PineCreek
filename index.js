0bfa85e88e7832cc65d7ca1cffc5282fe2596134
Lea Hughes <lahughesmusic@gmail.com>
2018-09-15 17:54:10 -0500

    port

--- index.js
+++ index.js
@@ -51,5 +51,5 @@ if(process.env.NODE_ENV === 'production'){
 const port = process.env.PORT || 5000;
 
 
-app.listen(port, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
+app.listen(port, '0.0.0.0', () => console.log(`Server running on port ${port}`));
 
