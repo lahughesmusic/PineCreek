65e49277990c0262b39c411723fe7914d403fb97
Lea Hughes <lahughesmusic@gmail.com>
2018-09-15 18:48:50 -0500

    port

--- index.js
+++ index.js
@@ -41,10 +41,11 @@ app.use('/api/post', posts);
 if(process.env.NODE_ENV === 'production'){
   //set static folder
   app.use(express.static('client/build'));
+}
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   })
-}
+
 
 
 
