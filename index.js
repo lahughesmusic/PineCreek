a88246a6199830d2af3cad2e7a1011e8209bb927
Lea Hughes <lahughesmusic@gmail.com>
2018-09-15 17:34:35 -0500

    port issues

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ if(process.env.NODE_ENV === 'production'){
   })
 }
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
-app.listen(process.env.port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
 
