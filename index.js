d5e597a8de67da20a6f88d1bff85215f2c1cda89
Lea Hughes <lahughesmusic@gmail.com>
2018-09-16 15:09:26 -0500

    add files

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.get('*', (req, res) => {
 
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
