d8494070bf864861c2a3c214dde2454f26afa0a7
Lea Hughes <lahughesmusic@gmail.com>
2018-09-15 19:33:14 -0500

    add files

--- index.js
+++ index.js
@@ -37,20 +37,18 @@ require('./config/passport')(passport);
 app.use('/api/users', users);
 app.use('/api/post', posts);
 
-//Server static assets if in production
-if(process.env.NODE_ENV === 'production'){
-  //set static folder
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
   app.use(express.static('client/build'));
-}
+
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  })
-
-
-
+  });
+}
 
 const port = process.env.PORT || 5000;
 
-
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
 
